ee30ed73eff476721878b956549212574e7bb8e9
Charles Duran <62945929+4Charlie4@users.noreply.github.com>
2022-10-20 16:22:36 -0400

    Manifest generates

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -2,6 +2,7 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const WebpackPwaManifest = require("webpack-pwa-manifest");
 const path = require("path");
 const { InjectManifest } = require("workbox-webpack-plugin");
+const { pathToFileURL } = require("url");
 
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
 // TODO: Add CSS loaders and babel to webpack.
@@ -25,6 +26,28 @@ module.exports = () => {
       new InjectManifest({
         swSrc: "./src/src-sw.js",
         swDest: "service-worker.js",
+      }),
+      new WebpackPwaManifest({
+        name: "T.B.T.E",
+        short_name: "Basic Editor",
+        description: "Take notes or write some code and save for later!",
+        background_color: "#42099e",
+        theme_color: "#42099e",
+        start_url: "./",
+        publicPath: "./",
+        icons: [
+          {
+            src: path.resolve("src/images/Charlie.png"),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join("assets", "icons"),
+          },
+          {
+            src: path.resolve("src/images/Charlie.png"),
+            size: "1024x1024",
+            destination: path.join("assets", "icons"),
+            purpose: "maskable",
+          },
+        ],
       }),
     ],
 
